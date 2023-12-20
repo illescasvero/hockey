c41dd3a6bc3a3a08f247e98aca5e797fb026f2ac
illescasvero <150083439+illescasvero@users.noreply.github.com>
2023-12-19 21:27:28 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+let btnLogo = document.getElementById('logo');
+let menuD = document.getElementById('nav-m');
+
+btnLogo.addEventListener('click', function(){
+    menuD.classList.toggle('mostrarM');
+});
+
+
+//console.log(menuD)
